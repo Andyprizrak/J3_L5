e27aa29ee03739ca351c2fdd29c54cb63b06c782
Andrey Kozlov <pilotc@bk.ru>
2020-05-28 15:17:31 +0600

    hw

--- src/ru/geekbrains/home_j3_lesson5/Car.java
+++ src/ru/geekbrains/home_j3_lesson5/Car.java
@@ -1,22 +1,28 @@
 package ru.geekbrains.home_j3_lesson5;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
-    static {
-        CARS_COUNT = 0;
-    }
+    static {CARS_COUNT = 0;}
     private Race race;
     private int speed;
     private String name;
+    private CountDownLatch cdlR;
+    private CountDownLatch cdlF;
+    private static CountDownLatch cdl = new CountDownLatch(Main.CARS_COUNT);
+
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch cdlR, CountDownLatch cdlF) {
         this.race = race;
         this.speed = speed;
+        this.cdlR = cdlR;
+        this.cdlF = cdlF;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -26,11 +32,19 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cdl.countDown();
+            cdlR.countDown();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this, i + 1, race.getStages().size());
+        }
+        cdlF.countDown();
     }
 }
--- src/ru/geekbrains/home_j3_lesson5/Stage.java
+++ src/ru/geekbrains/home_j3_lesson5/Stage.java
@@ -2,11 +2,6 @@ package ru.geekbrains.home_j3_lesson5;
 
 public abstract class Stage {
     protected int length;
-
     protected String description;
-
-    public String getDescription() {
-        return description;
-    }
-    public abstract void go(Car c);
+    public abstract void go(Car c, int stagePos, int stageCount);
 }
